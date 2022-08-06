fd8d88e8e1177445bf333e9266a56e5ce91bb2f7
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-08-05 17:44:17 -0700

    Configure to car parts store

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-    return (
-        <section className="hero">
-            <h2>Wisdom Books</h2>
-            <h3>A room without books is like a <br />body without a soul</h3>
-            <Link className="btn" to="/books">View All Books</Link>
-        </section>
-    )
-}
+  return (
+    <section className="hero">
+      <h2>Contuso Car Parts Store</h2>
+      <br />
+      <br />
+      <Link className="btn" to="/books">
+        View All Car Parts
+      </Link>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
